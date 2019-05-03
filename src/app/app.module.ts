0241bcd2c2ce51575ed80d968369892782532bcb
bzhao16834 <bzhao16834@gmail.com>
2019-05-03 00:37:09 -0500

    Check point #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,33 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { initialState, reducers } from './app.state';
+
 import { AppRoutes } from './app.routing';
 import { AppComponent } from './app.component';
 import { BananaComponent } from './banana/banana.component';
 
 @NgModule({
-  imports:      [ 
-    BrowserModule, 
+  imports: [
+    BrowserModule,
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
+    StoreModule.forRoot(reducers, { initialState }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    }),
     RouterModule.forRoot(AppRoutes)
   ],
-  declarations: [ 
-    AppComponent, 
-    BananaComponent 
+  declarations: [
+    AppComponent,
+    BananaComponent
   ],
-  bootstrap: [ 
-    AppComponent 
+  bootstrap: [
+    AppComponent
   ]
 })
 export class AppModule { }
--- src/app/banana/banana.component.ts
+++ src/app/banana/banana.component.ts
@@ -1,35 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState, getMyBanana } from '../app.state';
+import { GetNewBanana } from './state';
 
 @Component({
-    selector: 'app-banana',
-    templateUrl: './banana.component.html',
-    styleUrls: ['./banana.component.css']
+  selector: 'app-banana',
+  templateUrl: './banana.component.html',
+  styleUrls: ['./banana.component.css']
 })
 export class BananaComponent implements OnInit {
 
-    title = 'My NgRx Banana App';
+  title = 'My NgRx Banana App';
 
-    constructor() {
+  banana$: Observable<any>;
 
-    }
+  constructor(private store: Store<AppState>) { }
 
-    ngOnInit() {
+  ngOnInit() {
+    this.newBanana();
+    this.banana$ = this.store.pipe(select(getMyBanana));
+  }
 
-    }
+  newBanana() {
+    this.store.dispatch(new GetNewBanana(null));
+  }
 
-    newBanana() {
+  peelBanana() {
 
-    }
+  }
 
-    peelBanana() {
+  eatBanana() {
 
-    }
+  }
 
-    eatBanana() {
+  timeHop() {
 
-    }
-
-    timeHop() {
-
-    }
+  }
 }
